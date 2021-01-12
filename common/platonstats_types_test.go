0ad45d1c5f7b0b66833194070b9c35e47ee9b06b
bglmmz <lvxiaoyi@juzix.net>
2021-01-12 18:05:48 +0800

    update unit test

--- common/platonstats_types_test.go
+++ common/platonstats_types_test.go
@@ -102,5 +102,18 @@ func buildExeBlockData() *ExeBlockData {
 	CollectEmbedTransferTx(blockNumber, Hash{0x01}, address, address, value1)
 	CollectEmbedContractTx(blockNumber, Hash{0x03}, address, address, Hex2Bytes("f855838203ec8180b842b84027eff1a24cfd76e7151c0410d6a2c9fe9660b0906862fe3c461f5bae4ce5893d4f975239187510ff4b399ec1daea4f71690e00de3de0c56a2bd896cf5cd37eca8a8902b5e3af16b1880000"))
 	CollectStakingSetting(blockNumber, big.NewInt(1000000))
+
+	fixDelegation := new(FixDelegation)
+	fixDelegation.NodeID = nodeId
+	fixDelegation.StakingBlockNumber = 11
+	fixDelegation.Withdraw = false
+	CollectFixDelegation(blockNumber, address, fixDelegation)
+
+	fixDelegation2 := new(FixDelegation)
+	fixDelegation2.NodeID = nodeId
+	fixDelegation2.StakingBlockNumber = 112000
+	fixDelegation2.Withdraw = true
+	CollectFixDelegation(blockNumber, address, fixDelegation2)
+
 	return GetExeBlockData(blockNumber)
 }
