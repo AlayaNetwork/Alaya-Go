cd6b554a672f9d20c42b8103d3f2a603a624aa10
ximi <ximili@yunify.com>
2021-06-21 16:52:43 +0800

    Election adds shuffling algorithm and adjust expectations

--- x/xcom/common_config.go
+++ x/xcom/common_config.go
@@ -63,6 +63,8 @@ const (
 	IncreaseIssuanceRatioLowerLimit   = 0
 
 	// When electing consensus nodes, it is used to calculate the P value of the binomial distribution
+	ElectionBase = 25	// New expectations
+
 	ElectionBaseL1 = 3000
 	ElectionBaseL2 = 6000
 
@@ -869,4 +871,8 @@ func CalcP(totalWeight float64, sqrtWeight float64) float64 {
 	} else {
 		return float64(ElectionBaseL2) / sqrtWeight
 	}
+}
+
+func CalcPNew(sqrtWeight float64) float64 {
+	return float64(ElectionBase) / sqrtWeight
 }
