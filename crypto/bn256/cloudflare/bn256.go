284d0e7a43169c70e5d135a80ebf0c2080781182
lishuguang <li_shu_guang@yeah.net>
2021-08-09 13:47:44 +0800

    修复bn256bug增加bn256的空检查

--- crypto/bn256/cloudflare/bn256.go
+++ crypto/bn256/cloudflare/bn256.go
@@ -100,6 +100,10 @@ func (e *G1) Marshal() []byte {
 	// Each value is a 256-bit number.
 	const numBytes = 256 / 8
 
+	if e.p == nil {
+		e.p = &curvePoint{}
+	}
+
 	e.p.MakeAffine()
 	ret := make([]byte, numBytes*2)
 	if e.p.IsInfinity() {
@@ -382,6 +386,10 @@ func (e *GT) Marshal() []byte {
 	// Each value is a 256-bit number.
 	const numBytes = 256 / 8
 
+	if e.p == nil {
+		e.p = &gfP12{}
+	}
+	
 	ret := make([]byte, numBytes*12)
 	temp := &gfP{}
 
