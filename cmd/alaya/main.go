cf6c50fe2d4802d8e0a526304f2448f45e34795d
liangqin.fan <liangqin.fan@gmail.com>
2021-08-18 14:23:10 +0800

    cmd/utils: allow for multiple influxdb tags (#18520)
    https://github.com/ethereum/go-ethereum/pull/18520

--- cmd/alaya/main.go
+++ cmd/alaya/main.go
@@ -142,7 +142,7 @@ var (
 		utils.MetricsInfluxDBDatabaseFlag,
 		utils.MetricsInfluxDBUsernameFlag,
 		utils.MetricsInfluxDBPasswordFlag,
-		utils.MetricsInfluxDBHostTagFlag,
+		utils.MetricsInfluxDBTagsFlag,
 	}
 
 	//mpcFlags = []cli.Flag{
