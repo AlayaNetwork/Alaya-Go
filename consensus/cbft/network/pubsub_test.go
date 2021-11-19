70b72f48ee9e4441e1a56e50a369ac60a9877668
WeiLoy <wei_loy@163.com>
2021-11-19 16:18:38 +0800

    Message sending of different topics between multiple nodes

--- consensus/cbft/network/pubsub_test.go
+++ consensus/cbft/network/pubsub_test.go
@@ -65,11 +65,11 @@ func (rw *TestPSRW) WriteMsg(msg p2p.Msg) error {
 	return nil
 }
 
-func TestPubSubPublish(t *testing.T) {
-	type TestMsg struct {
-		Title string
-	}
+type TestMsg struct {
+	Title string
+}
 
+func TestPubSubPublish(t *testing.T) {
 	expect := []*TestMsg{
 		{
 			Title: "n1_msg",
@@ -152,5 +152,132 @@ func TestPubSubPublish(t *testing.T) {
 }
 
 func TestPubSubPublish_DifferentTopics(t *testing.T) {
+	expect := []*TestMsg{
+		{
+			Title: "n1_msg",
+		},
+		{
+			Title: "n2_msg",
+		},
+		{
+			Title: "n3_msg",
+		},
+	}
 
+	wg := sync.WaitGroup{}
+	wg.Add(len(expect))
+	pubSub1 := makePubSub(func(p *peer, msg *p2p.Msg) error {
+		var tm TestMsg
+		err := msg.Decode(&tm)
+		if tm.Title != expect[1].Title && tm.Title != expect[2].Title {
+			t.Fatal("Expected value not received")
+		}
+		wg.Done()
+		t.Log("pubSub1 receive:", "data", tm, "err", err)
+		return nil
+	})
+	pubSub2 := makePubSub(func(p *peer, msg *p2p.Msg) error {
+		var tm TestMsg
+		err := msg.Decode(&tm)
+		assert.Equal(t, tm.Title, expect[0].Title)
+		wg.Done()
+		t.Log("pubSub2 receive:", "data", tm, "err", err)
+		return nil
+	})
+	pubSub3 := makePubSub(func(p *peer, msg *p2p.Msg) error {
+		var tm TestMsg
+		err := msg.Decode(&tm)
+		assert.Equal(t, tm.Title, expect[0].Title)
+		wg.Done()
+		t.Log("pubSub3 receive:", "data", tm, "err", err)
+		return nil
+	})
+
+	n1_2Chan := make(chan p2p.Msg)
+	n2_1Chan := make(chan p2p.Msg)
+	trw1 := &TestPSRW{
+		writeMsgChan: n1_2Chan,
+		readMsgChan:  n2_1Chan,
+	}
+	trw2 := &TestPSRW{
+		writeMsgChan: n2_1Chan,
+		readMsgChan:  n1_2Chan,
+	}
+
+	n1_3Chan := make(chan p2p.Msg)
+	n3_1Chan := make(chan p2p.Msg)
+	trw1_3 := &TestPSRW{
+		writeMsgChan: n1_3Chan,
+		readMsgChan:  n3_1Chan,
+	}
+	trw3_1 := &TestPSRW{
+		writeMsgChan: n3_1Chan,
+		readMsgChan:  n1_3Chan,
+	}
+
+	// connect peer
+	// peer1 <-> peer2
+	go func() {
+		newPeer := p2p.NewPeer(pubSub2.pss.Host().ID().ID(), "n2", nil)
+		t.Log("newPeer", "id", newPeer.ID().TerminalString(), "localId", pubSub1.pss.Host().ID().ID().TerminalString())
+		if err := pubSub1.handler(newPeer, trw1); err != nil {
+			t.Fatal(err)
+		}
+	}()
+	go func() {
+		newPeer := p2p.NewPeer(pubSub1.pss.Host().ID().ID(), "n1", nil)
+		t.Log("newPeer", "id", newPeer.ID().TerminalString(), "localId", pubSub2.pss.Host().ID().ID().TerminalString())
+		if err := pubSub2.handler(newPeer, trw2); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	// peer1 <-> peer3
+	go func() {
+		newPeer := p2p.NewPeer(pubSub3.pss.Host().ID().ID(), "n3", nil)
+		t.Log("newPeer", "id", newPeer.ID().TerminalString(), "localId", pubSub1.pss.Host().ID().ID().TerminalString())
+		if err := pubSub1.handler(newPeer, trw1_3); err != nil {
+			t.Fatal(err)
+		}
+	}()
+	go func() {
+		newPeer := p2p.NewPeer(pubSub1.pss.Host().ID().ID(), "n1", nil)
+		t.Log("newPeer", "id", newPeer.ID().TerminalString(), "localId", pubSub3.pss.Host().ID().ID().TerminalString())
+		if err := pubSub3.handler(newPeer, trw3_1); err != nil {
+			t.Fatal(err)
+		}
+	}()
+	time.Sleep(time.Millisecond * 6)
+
+	topic1 := "test1"
+	topic2 := "test2"
+	go func() {
+		if err := pubSub1.Subscribe(topic1); err != nil {
+			t.Fatal(err)
+		}
+		if err := pubSub1.Subscribe(topic2); err != nil {
+			t.Fatal(err)
+		}
+		time.Sleep(time.Millisecond * 3)
+		pubSub1.Publish(topic1, uint64(1), expect[0])
+	}()
+	go func() {
+		if err := pubSub2.Subscribe(topic1); err != nil {
+			t.Fatal(err)
+		}
+		time.Sleep(time.Millisecond * 3)
+		pubSub2.Publish(topic1, uint64(2), expect[1])
+	}()
+	go func() {
+		if err := pubSub3.Subscribe(topic2); err != nil {
+			t.Fatal(err)
+		}
+		time.Sleep(time.Millisecond * 3)
+		pubSub3.Publish(topic2, uint64(3), expect[2])
+	}()
+	wg.Wait()
+	pubSub1.Cancel(topic1)
+	pubSub1.Cancel(topic2)
+	pubSub2.Cancel(topic1)
+	pubSub3.Cancel(topic2)
 }
