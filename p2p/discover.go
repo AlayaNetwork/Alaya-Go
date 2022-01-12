b77b45e3603d624e3c490990e5f6afcb20f78bf7
clearly <910372762@qq.com>
2022-01-12 15:16:39 +0800

    update log

--- p2p/discover.go
+++ p2p/discover.go
@@ -65,7 +65,7 @@ func (srv *Server) DiscoverTopic(ctx context.Context, topic string) {
 					if peerNeedFind > len(copyNodes) {
 						peerNeedFind = len(copyNodes)
 					}
-					log.Debug("not enough nodes in this topic,searching network", "topic", topic, "peers", len(peers), "peerNeedFind", peerNeedFind, "minimumPeersPerTopic", srv.Config.MinimumPeersPerTopic)
+					log.Debug("not enough nodes in this topic,searching network", "topic", topic, "peers", len(peers), "remainNodes", len(copyNodes), "peerNeedFind", peerNeedFind, "minimumPeersPerTopic", srv.Config.MinimumPeersPerTopic)
 					if err := srv.FindPeersWithTopic(ctx, topic, copyNodes, peerNeedFind); err != nil {
 						log.Debug("Could not search for peers", "err", err)
 						return
