6a85232d505ee87ea74e52d1aa595e0531b8d2aa
luowei <luowei@juzix.net>
2022-02-23 15:22:45 +0800

    Modify the way the test is executed

--- build/ci.go
+++ build/ci.go
@@ -354,7 +354,7 @@ func doTest(cmdline []string) {
 	// and some tests run into timeouts under load.
 	gotest.Args = append(gotest.Args, "-p", "1")
 	if *coverage {
-		gotest.Args = append(gotest.Args, "-covermode=atomic", "-cover")
+		gotest.Args = append(gotest.Args, "-covermode=count", "-coverprofile=coverage.txt")
 	}
 	gotest.Args = append(gotest.Args, "-tags=test")
 	gotest.Args = append(gotest.Args, packages...)
